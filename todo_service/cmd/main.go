840c36e5af2ab4ce841b3599e4f6e81ced408edb
Madiyor <abdukhashimov@yandex.ru>
2020-09-07 17:51:17 +0500

    removed the todos

--- todo_service/cmd/main.go
+++ todo_service/cmd/main.go
@@ -1,10 +1,8 @@
 package main
 
-// @todo Implement the config
 // @body It is important to get basic defaults and to read the envs
 // @todo implement the logger
 // @todo implement integrate zapper
-// @todo implement etc
 // @todo implement service todo
 // @todo implement the storage
 // @todo implement the repo
