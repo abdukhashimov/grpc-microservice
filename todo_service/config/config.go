288bb08c9bef4d722e4f6ac3f50f6afd12275573
Madiyor <abdukhashimov@yandex.ru>
2020-09-07 16:59:33 +0500

    Added the load and getenvorDefault

---
+++ todo_service/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"os"
+
+	"github.com/spf13/cast"
+)
+
+// Base configuration struct
+type Config struct {
+	Environment string // development, staging, production
+	Lang        string // uz, ru, en
+
+	CassandraHost     string
+	CassandraPort     int
+	CassandraKeyspace string
+	CassandraPassword string
+	CassandraUser     string
+
+	LogLevel string
+	RPCPort  string
+}
+
+func Load() Config {
+	c := Config{}
+	c.Environment = cast.ToString(getOrReturnDefault("ENVIRONMENT", "development"))
+	c.Lang = cast.ToString(getOrReturnDefault("DEFAULT_LANG", "ru"))
+
+	c.CassandraHost = cast.ToString(getOrReturnDefault("CASSANDRA_HOST", "localhost"))
+	c.CassandraPort = cast.ToInt(getOrReturnDefault("CASSANDRA_PORT", 9042))
+	c.CassandraKeyspace = cast.ToString(getOrReturnDefault("CASSANDRA_KEYSPACE", "todo_service"))
+	c.CassandraPassword = cast.ToString(getOrReturnDefault("CASSANDRA_PASSWORD", "cassandra"))
+	c.CassandraUser = cast.ToString(getOrReturnDefault("CASSANDRA_USER", "cassandra"))
+
+	c.LogLevel = cast.ToString(getOrReturnDefault("LOG_LEVEL", "debug"))
+	c.RPCPort = cast.ToString(getOrReturnDefault("RPC_PORT", ":8005"))
+	return c
+
+}
+
+func getOrReturnDefault(key string, defaultValue interface{}) interface{} {
+	_, exists := os.LookupEnv(key)
+	if exists {
+		return os.Getenv(key)
+	}
+	return defaultValue
+}
--- todo_service/go.mod
+++ todo_service/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/gocql/gocql v0.0.0-20200815110948-5378c8f664e9 // indirect
 	github.com/golang/protobuf v1.4.2
 	github.com/scylladb/gocqlx/v2 v2.1.0 // indirect
+	github.com/spf13/cast v1.3.1
 	google.golang.org/protobuf v1.25.0
 )
