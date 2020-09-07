d2f71ed3cd690a34f177d55af7c013c6c066c43b
Madiyor <abdukhashimov@yandex.ru>
2020-09-07 17:37:28 +0500

    added the main todos

--- todo_service/cmd/main.go
+++ todo_service/cmd/main.go
@@ -2,6 +2,14 @@ package main
 
 // @todo Implement the config
 // @body It is important to get basic defaults and to read the envs
+// @todo implement the logger
+// @todo implement integrate zapper
+// @todo implement etc
+// @todo implement service todo
+// @todo implement the storage
+// @todo implement the repo
+// @todo implement the storage main
+// @todo try the api gateway
 func main() {
 
 }
