7e2e37feb4da47c9dd8faef1bd8a2acb8db402e9
Madiyor <abdukhashimov@yandex.ru>
2020-09-07 17:53:22 +0500

    added the debugging levels for log

---
+++ todo_service/pkg/logger/const.go
@@ -0,0 +1,11 @@
+package logger
+
+// Main constants that are used to debug the running code
+const (
+	LevelDebug = "debug"
+	LevelInfo  = "info"
+	LevelWarn  = "warn"
+	LevelError = "error"
+	LevelPanic = "panic"
+	LevelFatal = "fatal"
+)
