77136cfbc57e374733fc67665924381a66c86df4
Madiyor <abdukhashimov@yandex.ru>
2020-09-10 10:17:22 +0500

    implemented the create

--- todo_service/storage/cassandra/todo.go
+++ todo_service/storage/cassandra/todo.go
@@ -1,9 +1,38 @@
 package cassandra
 
 import (
+	"time"
+
+	"github.com/abdukhashimov/blogcas/todo_service/todopb"
 	"github.com/gocql/gocql"
 )
 
 type todoRepo struct {
 	session *gocql.Session
+}
+
+func (repo *todoRepo) Create(todo *todopb.Todo) (string, error) {
+	id, err := gocql.RandomUUID()
+	if err != nil {
+		return "", nil
+	}
+
+	query := repo.session.Query(`INSERT INTO todos (
+		id,
+		title,
+		description,
+		done,
+		created_at,
+		updated_at,
+	) VALUES (?, ?, ?, ?, ?, ?)`,
+		id,
+		todo.GetTitle(),
+		todo.GetDescription(),
+		time.Now(),
+		time.Now(),
+	)
+	if err := query.Exec(); err != nil {
+		return "", nil
+	}
+	return id.String(), nil
 }
